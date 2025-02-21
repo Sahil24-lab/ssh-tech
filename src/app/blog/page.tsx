f5a391a96c77c7285194fdcf8521506e561384cd
Sahil Harriram <sahil.harriram@gmail.com>
2025-02-21 17:53:14 +0700

    fixed build error

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <Layout>
       <h1>About Us</h1>
-      <p>Click "Home" in the header to test the loading spinner.</p>
+      <p>Click in the header to test the loading spinner.</p>
     </Layout>
   );
 }
