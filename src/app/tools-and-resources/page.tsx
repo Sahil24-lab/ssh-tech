8cb18a52175ccde0a33885d47c188fbd2afe5d73
Sahil Harriram <sahil.harriram@gmail.com>
2025-04-09 16:32:48 +0700

    udpated tools and blog page

--- src/app/tools-and-resources/page.tsx
+++ src/app/tools-and-resources/page.tsx
@@ -1,7 +1,6 @@
-import ConstrainedContainer from "@/components/layout/container/constrained-container";
 import Layout from "@/components/layout/Layout";
 import { Typography } from "@mui/material";
-
+import ConstrainedContainer from "@/components/layout/container/constrained-container";
 export default function AboutPage() {
   return (
     <Layout>
